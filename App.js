8f3e93df2b30b608d8b45af699e8899d9ef44ae7
JuyoungKwak0618 <rmmr123@naver.com>
2024-04-01 15:05:54 +0900

    React Native

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, StyleSheet, Text, View , Image} from 'react-native';
+import { ScrollView, StyleSheet, Text, View , Image, FlatList} from 'react-native';
 import HelloWorld from './compomnents/HelloWorld';
 import Cat from './compomnents/Cat';
 import State from './compomnents/State';
 import PizzaTranslator from './compomnents/PizzaTranslator';
+import FlatListBasic from './compomnents/FlatListBasic'
+import SectionListBasic from './compomnents/SectionListBasic';
+import FetchExample from './compomnents/FetchExample';
 
 const logo = { uri: 'https://reactnative.dev/img/tiny_logo.png' ,width: 32, height: 32,};
 
 export default function App() {
   return (
     <ScrollView>
+      <Image source={logo} />
+        <FetchExample />
+      <Image source={logo} />
+        <SectionListBasic />
+        <FlatListBasic />
       <Image source={logo} />
        <PizzaTranslator />
         <State name='Run Tum Tugge' />
---
+++ compomnents/FlatListBasic.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import {StyleSheet, View, Text, FlatList } from "react-native";
+
+
+const styles = StyleSheet.create({
+  container: {flex:1, paddingTop: 20},
+  item: {padding:10, fontSize:20, height:40}
+})
+
+export default function FlatListBasic() {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize:30, padding:15, fontWeight:'bold'}}>내가 좋아하는 과일</Text>
+      <FlatList
+        data={[
+          {key: 'Apple'},
+          {key: 'Banana'},
+          {key: 'Cherry'},
+          {key:'Orange'},
+          {key:'Mango'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
+};
